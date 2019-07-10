d69177f6bdc4a4d0c72e49e3a435bfb4ef6f9aae
Esther Estrada <esther.estrada@ujcv.edu.hn>
2019-07-09 20:17:31 -0600

    el main no funciona :(

--- src/edu/ujcv/progra2/matriz/IGMatR2.java
+++ src/edu/ujcv/progra2/matriz/IGMatR2.java
@@ -21,27 +21,32 @@ public class IGMatR2 {
 
 
 
-//    public IGMatR2 mul(IGMatR2 b){
-//
-//    }
-//
+    //public IGMatR2 mul(IGMatR2 b) {
+    //}
 
 
-//    public IGVecR2 colXMat(IGVecR2 col){
-//
-//    }
 
-//    public IGMatR2 suma (IGMatR2 b){
-//
-//    }
+    //public IGVecR2 colXMat(IGVecR2 col){
 
-//    public IGMatR2 escalarXmat(double escalar){
-//
-//    }
+    //}
 
-//    public static IGVecR2 rotacion (double angulo, IGVecR2 vector){
-//
-//    }
+    //public IGMatR2 suma (IGMatR2 b){
+
+    //}
+
+  //  public IGMatR2 escalarXmat(double escalar){
+    //    double retval;
+      //  return retval;
+    //}
+
+    public IGMatR2 rotacion(double angulo, IGVecR2 vector){
+        IGMatR2 retval = new IGMatR2();
+        retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+        retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+        retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+        retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+         return retval;
+        }
 
 
     public IGVecR2 getF1() {
