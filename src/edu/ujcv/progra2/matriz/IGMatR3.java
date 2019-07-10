b21a73df3444751a15db75148fad6d4f3ecf5c00
Esther Estrada <esther.estrada@ujcv.edu.hn>
2019-07-09 20:35:53 -0600

    me rindo

--- src/edu/ujcv/progra2/matriz/IGMatR3.java
+++ src/edu/ujcv/progra2/matriz/IGMatR3.java
@@ -1,18 +1,35 @@
 package edu.ujcv.progra2.matriz;
 
+import edu.ujcv.progra2.vector.IGVecR3;
+
 public class IGMatR3 {
 
 
-    // public static IGVecR2 rotacionZ (double angulo, IGVecR2 vector){
-//
-//    }
+    public static IGVecR3 rotacionZ (double angulo, IGVecR3 vector){
+        IGMatR2 retval = new IGMatR2();
+        retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+        retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+        retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+        retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+        return retval;
+     }
 
-    // public static IGVecR2 rotacionX (double angulo, IGVecR2 vector){
-//
-//    }
+     public static IGVecR3 rotacionX (double angulo, IGVecR3 vector){
+        IGMatR2 retval = new IGMatR2();
+        retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+        retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+        retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+        retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+        return retval;
+    }
 
-    // public static IGVecR2 rotacionY (double angulo, IGVecR2 vector){
-//
-//    }
+     public static IGVecR3 rotacionY (double angulo, IGVecR3 vector){
+        IGMatR2 retval = new IGMatR2();
+        retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+        retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+        retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+        retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+         return retval;
+   }
 
 }
--- src/edu/ujcv/progra2/matriz/IGMatR4.java
+++ src/edu/ujcv/progra2/matriz/IGMatR4.java
@@ -1,17 +1,39 @@
 package edu.ujcv.progra2.matriz;
 
+import edu.ujcv.progra2.vector.IGVecR2;
+
 public class IGMatR4 {
 
-    // public static IGVecR2 rotacionZ (double angulo, IGVecR2 vector){
-//
-//    }
+    IGVecR2 f1;
+    IGVecR2 f2;
+    IGVecR2 f3;
+    IGVecR2 f4;
 
-    // public static IGVecR2 rotacionX (double angulo, IGVecR2 vector){
-//
-//    }
+     public IGVecR2 rotacionZ(double angulo, IGVecR2 vector){
+         IGMatR2 retval = new IGMatR2();
+         retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+         retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+         retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+         retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+         return retval;
+    }
 
-    // public static IGVecR2 rotacionY (double angulo, IGVecR2 vector){
-//
-//    }
+     public IGVecR2 rotacionX (double angulo, IGVecR2 vector){
+         IGMatR2 retval = new IGMatR2();
+         retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+         retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+         retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+         retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+         return retval;
+    }
+
+     public static IGVecR2 rotacionY (double angulo, IGVecR2 vector){
+         IGMatR2 retval = new IGMatR2();
+         retval.f1.setX(this.f1.getX() * Math.cos(angulo));
+         retval.f1.setY(this.f1.getY() * (-Math.sin(angulo)));
+         retval.f2.setX(this.f2.getX() * Math.cos(angulo));
+         retval.f2.setY(this.f2.getY() * Math.sin(angulo));
+         return retval;
+    }
 
 }
